05ac7a4651fcbdb60e3449885ad620af0f52a549
You Thought <56780234+itamar1238@users.noreply.github.com>
2022-04-08 13:35:04 +0000

    fixed things

--- static/keyp.js
+++ static/keyp.js
@@ -1,5 +1,13 @@
+function getCookie(name) {
+    var match = document.cookie.match(RegExp('(?:^|;\\s*)' + name + '=([^;]*)')); 
+    return match ? match[1] : null;
+}
+
 $(document).keydown(function(e) {
     if(e.keyCode === 192) {
-        window.open("https://classroom.google.com")
+        if (getCookie('panic') == null) {
+          document.cookie = "panic=classroom.google.com;"
+        }
+        window.open("https://" + getCookie('panic'))
     }
 });
